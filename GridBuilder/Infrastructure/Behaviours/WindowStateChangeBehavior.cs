c7de86c895b17d49101fd86395228ae0a1327d96
Konrad Witczak <konwit18@gmail.com>
2021-07-03 21:25:18 +0200

    old

---
+++ GridBuilder/Infrastructure/Behaviours/WindowStateChangeBehavior.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace GridBuilder.Infrastructure.Behaviours
+{
+    class WindowStateChangeBehavior : Behavior<Button>
+    {
+        
+        protected override void OnAttached() => AssociatedObject.Click += OnButtonClick;
+
+        protected override void OnDetaching() => AssociatedObject.Click -= OnButtonClick;
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!(AssociatedObject.FindVisualRoot() is Window window)) return;
+            switch(window.WindowState)
+            {
+                case WindowState.Normal:
+                    window.WindowState = WindowState.Minimized;
+                    break;
+            }
+        }
+
+    }
+}
+
---
+++ GridBuilder/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,115 @@
+﻿using GridBuilder.Models;
+using GridBuilder.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GridBuilder.ViewModels
+{
+    class MainWindowViewModel
+    {
+        VisualGridModel visualGridModel = new VisualGridModel();
+        public List<(int, double)> ListValueX
+        {
+            get => visualGridModel.ListStepsX;
+            set
+            {
+                visualGridModel.ListStepsX = value;
+                visualGridModel.SetTransform(GetParentSize().Item1, GetParentSize().Item2);
+            }
+        }
+        public List<(int, double)> ListValueY
+        {
+            get => visualGridModel.ListStepsY;
+            set
+            {
+                visualGridModel.ListStepsY = value;
+                visualGridModel.SetTransform(GetParentSize().Item1, GetParentSize().Item2);
+            }
+        }
+        public List<double> ListValueZ
+        {
+            get => visualGridModel.ListStepsZ;
+            set
+            {
+                visualGridModel.ListStepsZ = value;
+                visualGridModel.SetTransform(GetParentSize().Item1, GetParentSize().Item2);
+            }
+        }
+        public List<string> ListMarkX
+        {
+            get => visualGridModel.ListMarkX;
+            set
+            {
+                visualGridModel.ListMarkX = value;
+                visualGridModel.SetTransform(GetParentSize().Item1, GetParentSize().Item2);
+            }
+        }
+        public List<string> ListMarkY
+        {
+            get => visualGridModel.ListMarkY;
+            set
+            {
+                visualGridModel.ListMarkY = value;
+                visualGridModel.SetTransform(GetParentSize().Item1, GetParentSize().Item2);
+            }
+        }
+        public List<string> ListMarkZ
+        {
+            get => visualGridModel.ListMarkZ;
+            set
+            {
+                visualGridModel.ListMarkZ = value;
+                visualGridModel.SetTransform(GetParentSize().Item1, GetParentSize().Item2);
+            }
+        }
+
+        public GeometryGroup GridGeometr { get => visualGridModel.Geometry; }
+        private (double, double) GetParentSize()
+        {
+            var win = Application.Current.MainWindow;
+            var grArray = Extensions.FindVisualChildren<Grid>(win);
+            Grid grid = grArray.Where(n => n.Name == "grid_Parent").FirstOrDefault();
+            return (grid.ActualWidth, grid.ActualHeight); 
+        }
+
+        private RelayCommand resizeCommand;
+        public RelayCommand ResizeCommand
+        {
+            get
+            {
+                return resizeCommand ??
+                    (resizeCommand = new RelayCommand(obj =>
+                    {
+                        Grid gr = obj as Grid;
+                        double w = gr.ActualWidth;
+                        double h = gr.ActualHeight;
+                        visualGridModel.SetTransform(w, h);
+                    }));
+            }
+        }
+
+        private RelayCommand createTSObjectCommand;
+        public RelayCommand CreateTSObjectCommand
+        {
+            get
+            {
+                return createTSObjectCommand ??
+                    (createTSObjectCommand = new RelayCommand(obj =>
+                    {
+                        TSGridModel gridModel = new TSGridModel(ListValueX, ListValueY, ListValueZ,
+                            ListMarkX, ListMarkY, ListMarkZ);
+                        gridModel.GridInsert();
+                    }));
+            }
+        }
+
+
+
+    }
+}
