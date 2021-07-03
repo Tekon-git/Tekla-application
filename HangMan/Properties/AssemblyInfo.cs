93caff7f11e9b94055ae77d7588a027be0bbe428
Konrad Witczak <konwit18@gmail.com>
2021-07-03 16:23:44 +0200

    First commit

--- HangMan/Properties/AssemblyInfo.cs
+++ HangMan/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConvertPlate")]
+[assembly: AssemblyTitle("HangMan")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConvertPlate")]
+[assembly: AssemblyProduct("HangMan")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c9706bc9-36f2-4aa6-9f54-b3f2daf195aa")]
+[assembly: Guid("ee015725-c1dd-480f-bb43-516ee2067562")]
 
 // Version information for an assembly consists of the following four values:
 //
